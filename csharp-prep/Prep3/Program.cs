37970a36c41012ade0304828cc7d61d57c7e4d2b
davidbolivar577 <david.bolivar577@gmail.com>
2026-01-12 13:22:29 -0700

    Finish Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool answer = true;
+        int count = 0;
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        do
+        {
+            count++;
+
+            Console.Write("What is your guess?: ");
+            int guess = int.Parse(Console.ReadLine());
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+                Console.WriteLine($"You made {count} guesses");
+                Console.Write("Do you want to play again? (y/n): ");
+                string cont = Console.ReadLine().ToLower();
+                if (cont.Substring(0, 1) == "y")
+                {
+                    magic = randomGenerator.Next(1, 101);
+                }
+                else
+                {
+                    answer = false;
+                }
+
+            }
+        }
+        while (answer);
     }
 }
