d19cdc0dd92aec6c147657d0f33639ac983a2f18
davidbolivar577 <david.bolivar577@gmail.com>
2026-01-22 10:25:52 -0700

    Finish Learning2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+        job1._company = "Company A";
+        job1._jobTitle = "Basic Job";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+
+        Job job2 = new();
+        job2._company = "Company B";
+        job2._jobTitle = "Slightly Better Job";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+        Resume resume = new();
+        resume._name = "David Bolivar";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
