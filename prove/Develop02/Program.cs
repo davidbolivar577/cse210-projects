76ca3f19665a3d27609786e6e44fc77e9f5a5f2a
davidbolivar577 <david.bolivar577@gmail.com>
2026-01-28 13:45:45 -0700

    Finish Develop2 logic

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,63 @@ using System;
 
 class Program
 {
+    public Journal current = new();
+    public List<String> prompts = [];
+
+    static void Write()
+    {
+        //give prompt
+        //receive answer
+        //add entry
+    }
+
+    static void Load()
+    {
+        Console.Write("Enter your filename: ");
+        current = Journal.Load($"{Console.ReadLine()}.csv");
+    }
+
+    static void Save()
+    {
+        Console.Write("Enter your filename: ");
+        current.Save($"{Console.ReadLine()}.csv");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool cont = true;
+        string choice = "";
+        Console.WriteLine("Welcome to Journal Program!");
+        do{
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            Console.Write("What would you like to do?: ");
+            choice = Console.ReadLine();
+
+            switch(choice)
+            {
+                case "1":
+                    Write();
+                    break;
+                
+                case "2":
+                    current.Display();
+                    break;
+                
+                case "3":
+                    Load();
+                    break;
+                
+                case "4":
+                    Save();
+                    break;
+                
+                case "5":
+                    cont = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+            }
+        }while(cont);
     }
 }
