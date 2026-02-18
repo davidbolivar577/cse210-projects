0fec69e66fa4d77f09382e75c4b71980480c73ba
davidbolivar577 <david.bolivar577@gmail.com>
2026-02-18 00:22:01 -0700

    Finish Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new("David 1", "Factoring");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new("David 2", "Calculus", "5.2", "1-17");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new("David 3", "Biology", "The ATP Cycle");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
