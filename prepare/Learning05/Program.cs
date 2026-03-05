b91d434ba5769deb9cf3b671333a5774436ab770
davidbolivar577 <david.bolivar577@gmail.com>
2026-03-05 09:47:37 -0700

    Finish Learning5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [];
+        shapes.Add(new Square("blue", 4.5));
+        shapes.Add(new Rectangle("red", 4, 5));
+        shapes.Add(new Circle("yellow", 4.5));
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+            Console.WriteLine();
+        }
     }
 }
